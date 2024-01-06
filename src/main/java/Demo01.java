791e3fc1c104f66e413176f5eb14ce8ddf38a0d8
Administrator <xiaoyi19n@126.com>
2024-01-06 14:01:41 +0800

    commit 11

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,12 @@
+/**
+ * @Author nfx
+ * @Create 2024/1/6
+ * Description:
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
